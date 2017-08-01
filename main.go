511344af33fa9e7ea1e4c9bb121c95b5ee76e260
Zack Scholl <zack.scholl@gmail.com>
2017-07-31 20:17:49 -0600

    Trying to do something

--- main.go
+++ main.go
@@ -6,51 +6,112 @@ import (
 	"io"
 	"os"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	// log.SetFormatter(&log.JSONFormatter{})
+	// log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
 
 func main() {
 	t := time.Now()
 	r, _ := os.Open("test.txt")
-	start := []byte(`rel="bookmark">`)
-	end := []byte(`</a>`)
-	b := make([]byte, len(start)*2+2)
-	capture := make([]byte, 1000)
-	captureLen := 0
-	capturing := false
-	for {
-		n, err := r.Read(b)
-		// fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		// fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
+	type Search struct {
+		After          []byte   // wait until `After` appears beforing looking for activator
+		Activators     [][]byte // wait until all these are seen to activate
+		Deactivators   [][]byte
+		Limit          int
+		Captured       [][]byte
+		numActivated   int
+		numDeactivated int
+		capture        []byte
+		i              int
+	}
+	s := Search{
+		After:          []byte("laskjdf"),
+		Activators:     [][]byte{[]byte(`<option class="level-0" `), []byte(`>`)},
+		Deactivators:   [][]byte{[]byte(`</option>`)},
+		Limit:          10,
+		Captured:       [][]byte{},
+		numActivated:   0,
+		numDeactivated: 0,
+		capture:        make([]byte, 50),
+		i:              0,
+	}
+
+	// Find largest activator/deactivator
+	maxSize := 0
+	for _, phrase := range append(s.Activators, s.Deactivators...) {
+		if len(phrase) > maxSize {
+			maxSize = len(phrase)
 		}
-		if !capturing {
-			index := bytes.Index(b, start)
-			if index > -1 {
-				capturing = true
-				for _, by := range b[index+len(start) : n] {
-					capture[captureLen] = by
-					captureLen++
+	}
+
+	bytesRead := make([]byte, maxSize*2+2)
+	for {
+		n, errRead := r.Read(bytesRead)
+		indexActive := -1
+
+		// Only try to activate if there's still things to check
+		if s.numActivated < len(s.Activators) {
+			indexActive = bytes.Index(bytesRead, s.Activators[s.numActivated])
+			if indexActive > -1 {
+				log.Debug("Activating")
+				s.numActivated++
+			}
+		}
+
+		if len(s.Activators) == s.numActivated && len(s.Captured) < s.Limit {
+
+			// Check if we should deactivate
+			indexDeactivate := bytes.Index(bytesRead, s.Deactivators[s.numDeactivated])
+			if indexDeactivate > -1 {
+				log.Debug("Deactivating")
+				s.numDeactivated++
+				if len(s.Deactivators) == s.numDeactivated {
+					// add bytes up to deactivator
+					for _, b := range bytesRead[n-indexDeactivate:] {
+						s.capture[s.i] = b
+						s.i++
+					}
+					log.Debugf("Added %d bytes", n-indexDeactivate)
+
+					// add and reset
+					s.Captured = append(s.Captured, s.capture[:s.i])
+					s.numActivated = 0
+					s.numDeactivated = 0
+					s.i = 0
+					s.capture = make([]byte, 1000)
 				}
 			}
-		} else {
-			index := bytes.Index(b, end)
-			if index > -1 {
-				capturing = false
-				for _, by := range b[:index] {
-					capture[captureLen] = by
-					captureLen++
+
+			// if not reset, then add bytes
+			if s.numActivated == len(s.Activators) {
+				if indexActive > -1 {
+					indexActive += len(s.Activators[s.numActivated-1])
+				} else {
+					indexActive = 0
 				}
-				break
-			} else {
-				for _, by := range b {
-					capture[captureLen] = by
-					captureLen++
+				for _, b := range bytesRead[indexActive:] {
+					s.capture[s.i] = b
+					s.i++
 				}
+				log.Debugf("Added %d bytes", n)
 			}
+
+		}
+
+		if errRead == io.EOF {
+			break
 		}
 	}
 	r.Close()
-	fmt.Println(string(capture[:captureLen]))
+	fmt.Printf("%+v", s)
 	fmt.Println(time.Since(t))
+	for _, r := range s.Captured {
+		fmt.Println(string(r))
+	}
 }
